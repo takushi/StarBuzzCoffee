acd8fa36f1fe1dbffae9f744489aa95281d490da
takushi <takushi@mfmf.me>
2015-08-02 16:29:22 +0900

    Initial Commit

---
+++ StarBuzzCoffee/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  StarBuzzCoffee
+//
+//  Created by Homma Takushi on 2015/08/02.
+//  Copyright (c) 2015年 mfmf.me. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
